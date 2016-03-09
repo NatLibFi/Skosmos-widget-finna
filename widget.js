f06148307903d35f5515872a5debfcdaf1205965
henriyli <henri.ylikotila@helsinki.fi>
2016-03-09 17:16:41 +0200

    transforming the widget into an accordion

--- widget.js
+++ widget.js
@@ -16,28 +16,37 @@ $(function() {
         $.getJSON(url, function(data) {
             if (data.resultCnt === 0) { return; }
             finnaResults = data; 
-            renderWidget(term);
+            renderWidget(finnaTerm, false);
         });
     }
 
     function updateResults() {
         $('.concept-widget').remove();
-        renderWidget(finnaTerm);
+        renderWidget(finnaTerm, true);
     }
 
-    function renderWidget(term) {
+    function renderWidget(term, isOpened) {
         var finnaUrl = 'https://www.finna.fi/Search/Results?' + $.param({lookfor: term, filter: ['usage_rights_str_mv:"usage_C"', 'online_boolean:1'], type: 'Subject'});
-        $('.content').append(Handlebars.compile($('#finna-template').html())({label: prefLabels[0].label, count: finnaResults.resultCount, finnalink: finnaUrl, records: finnaResults.records.slice(finnaOffset, finnaOffset + 5)}));
-        $('.concept-widget > .row > button:first').on('click', function() {
+        $('.content').append(Handlebars.compile($('#finna-template').html())({label: prefLabels[0].label, count: finnaResults.resultCount, finnalink: finnaUrl, records: finnaResults.records.slice(finnaOffset, finnaOffset + 5), opened: isOpened}));
+        $('#collapseOne > .panel-body > .row > button:first').on('click', function() {
             if (finnaOffset >= 5) {
                 finnaOffset -= 5;
                 updateResults();
             }
         });
-        $('.concept-widget > .row > button:last').on('click', function() {
+        $('#collapseOne > .panel-body > .row > button:last').on('click', function() {
             if ((finnaOffset + 5) < parseInt($('.count').html(), 10) && (finnaOffset + 5) < resultLimit) {
                 finnaOffset += 5;
                 updateResults();
+            }
+        });
+
+        $('#headingOne').on('click', function() { 
+            var $glyph = $(this).children().children('.glyphicon');
+            if ($glyph.hasClass('glyphicon-chevron-down')) {
+                $glyph.removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+            } else {
+                $glyph.removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
             }
         });
     }
