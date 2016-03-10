ba8e244fad8569ea4ad1e154c1cf98c4ce4c47e3
henriyli <henri.ylikotila@helsinki.fi>
2016-03-10 17:22:02 +0200

    adds semi functional paging, still needs an extra keypress to actually show the results...

--- widget.js
+++ widget.js
@@ -3,18 +3,22 @@ $(function() {
     var finnaOffset = 0;
     var finnaResults;
     var finnaTerm;
-    var resultLimit = 40;
+    var resultLimit = 10;
+    var resultsFetched = 0;
     var currentFormat = readCookie('FINNA_WIDGET_FORMAT') ? parseInt(readCookie('FINNA_WIDGET_FORMAT'), 10) : 1;
     var formats = ['', '~format:0/Image/', '~format:0/Book/', '~format:0/PhysicalObject/'];
     var formatName = [{fi: 'aineistoja (kaikki tyypit)', sv: '', en: 'records'}, {fi: 'kuva-aineistoja', sv: 'bild', en: 'image records'}, {fi: 'kirjoja', sv: 'böcker', en: 'books'}, {fi: 'esineitä'}];
 
-    if (uri) { // Using this to detect when on a Skosmos concept/group page since the uri variable will be undefined otherwise.
+    if (typeof uri !== 'undefined') { // Using this to detect when on a Skosmos concept/group page since the uri variable will be undefined otherwise.
         queryFinna(prefLabels[0].label, 0, 0);
     }
 
     function queryFinna(term, offset, limit) {
         finnaTerm = term;
         var params = {lookfor: term, limit: limit,filter: ['online_boolean:1', formats[currentFormat]], view: 'jsonp', type: 'Subject'};
+        if (offset) {
+            params.page = (offset / 5) + 1;
+        }
         if (currentFormat === 0) { 
             params.filter = ['online_boolean:1'];
         }
@@ -22,6 +26,7 @@ $(function() {
         $.getJSON(url, function(data) {
             if (data.resultCnt === 0) { return; }
             if (data.records) {
+                resultsFetched += data.records.length;
                 for (var i in data.records) {
                     if (data.records[i].id.indexOf('urn:nbn') !== -1) {
                         // for some reason the urn containing id's need to be double encoded...
@@ -29,7 +34,12 @@ $(function() {
                     }
                 }
             }
-            finnaResults = data; 
+            if (!finnaResults || typeof finnaResults.records === 'undefined') {
+                finnaResults = data; 
+            } else { 
+                // if there are already some records from previous searches appending the new results to that array
+                finnaResults.records = finnaResults.records.concat(data.records);
+            }
             var opened = (data.records !== undefined);
             renderWidget(finnaTerm, opened);
         });
@@ -52,9 +62,11 @@ $(function() {
                 }
             });
             $('#collapseFinna > .panel-body > .row > button:last').on('click', function() {
-                if ((finnaOffset + 5) < parseInt($('.count').html(), 10) && (finnaOffset + 5) < resultLimit) {
+                if ((finnaOffset + 5) < parseInt($('.count').html(), 10) && (finnaOffset + 5) < resultsFetched) {
                     finnaOffset += 5;
                     updateResults();
+                } else { // here we have reached the end of the current set of results
+                    queryFinna(prefLabels[0].label, finnaOffset, resultLimit)
                 }
             });
         } else {
